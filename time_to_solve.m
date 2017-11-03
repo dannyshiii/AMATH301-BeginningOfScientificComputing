1488b76200788ef2a191691f88295cc341993cd0
DannyShiii <dannyshisdn@gmail.com>
2017-11-02 20:47:43 -0700

    Update time_to_solve.m

--- time_to_solve.m
+++ time_to_solve.m
@@ -1,10 +1,9 @@
 function time = time_to_solve(N)
-
+% This function first generates a N*N matrix from the function rand.
+% And then returns the time of solving A\b with Gaussian Elimination for MATLAB
 A = rand(N);
 b = rand(N,1);
-
 tic;
 A\b;
 time = toc;
-
 end
